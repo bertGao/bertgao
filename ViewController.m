343761d0e9e6b32279f5affc1419094813b8aed2
bert <bert@bertdeMac-mini.local>
2017-04-27 10:57:38 +0800

    456

---
+++ ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  gertgao
+//
+//  Created by bert on 2017/4/27.
+//  Copyright © 2017年 bert. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"bertgao1");
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
